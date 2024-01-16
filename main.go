32c53f61af9f1f456c3ac88a6916916d55c259f6
Varun Chembu <varun@varunchembu.com>
2024-01-15 17:31:05 -0800

    First part

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Word Count TBD")
+	var useC bool
+
+	flag.BoolVar(&useC, "c", false, "Enable option C")
+
+	flag.Parse()
+	nonFlagArgs := flag.Args()
+	var filename string
+
+	// fmt.Println("Options:", options)
+	if len(nonFlagArgs) > 0 {
+		filename = nonFlagArgs[0]
+	} else {
+		fmt.Println("No filename provided.")
+	}
+
+	file, _ := os.Open(filename)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanBytes)
+
+	var byteCount int
+	for scanner.Scan() {
+		byteCount++
+	}
+
+	fmt.Println(byteCount, filename)
 }
