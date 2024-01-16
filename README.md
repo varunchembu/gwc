307c8f7270ceaafc9b9a62b57433b8726d26db40
Varun Chembu <varun@varunchembu.com>
2024-01-15 19:29:12 -0800

    Done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 wc clone in go
+
+Usage:
+
+./gwc -c test.txt
+./gwc -l test.text
+./gwc -w test.text
+./gwc -m test.text
+
+cat test.txt | ./gwc -l
--- main.go
+++ main.go
@@ -4,34 +4,102 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
-func main() {
-	var useC bool
+type Stats struct {
+	Lines int
+	Words int
+	Bytes int
+	Runes int
+}
 
-	flag.BoolVar(&useC, "c", false, "Enable option C")
+func main() {
+	var useC, useL, useW, useM bool
+
+	flag.BoolVar(&useC, "c", false, "Print byte count")
+	flag.BoolVar(&useL, "l", false, "Print line count")
+	flag.BoolVar(&useW, "w", false, "Print word count")
+	flag.BoolVar(&useM, "m", false, "Print rune count")
 
 	flag.Parse()
-	nonFlagArgs := flag.Args()
+
 	var filename string
-
-	// fmt.Println("Options:", options)
-	if len(nonFlagArgs) > 0 {
+	if nonFlagArgs := flag.Args(); len(nonFlagArgs) > 0 {
 		filename = nonFlagArgs[0]
+	}
+	lineCount, wordCount, runeCount, byteCount := countStats(filename)
+
+	stats := Stats{
+		Lines: lineCount,
+		Words: wordCount,
+		Bytes: byteCount,
+		Runes: runeCount,
+	}
+
+	printResults(stats, filename, useL, useW, useC, useM)
+
+}
+
+func countStats(filename string) (lineCount, wordCount, runeCount, byteCount int) {
+
+	var reader io.Reader
+
+	if filename == "" {
+		reader = os.Stdin
 	} else {
-		fmt.Println("No filename provided.")
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error opening file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		reader = file
 	}
 
-	file, _ := os.Open(filename)
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanBytes)
-
-	var byteCount int
-	for scanner.Scan() {
-		byteCount++
+	bufReader := bufio.NewReader(reader)
+	for {
+		line, err := bufReader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			fmt.Fprintf(os.Stderr, "error reading file: %v\n", err)
+			os.Exit(1)
+		}
+		if line != "" {
+			lineCount++
+			wordCount += len(strings.Fields(line))
+			runeCount += utf8.RuneCountInString(line)
+			byteCount += len(line)
+		}
+		if err == io.EOF {
+			break
+		}
 	}
 
-	fmt.Println(byteCount, filename)
+	return
+}
+
+func printResults(stats Stats, filename string, useL, useW, useC, useM bool) {
+
+	lineCount, wordCount, runeCount, byteCount := stats.Lines, stats.Words, stats.Runes, stats.Bytes
+	if !(useC || useL || useW || useM) {
+		fmt.Printf("%d %d %d %s\n", lineCount, wordCount, byteCount, filename)
+		return
+	}
+	if useL {
+		fmt.Printf("%d ", lineCount)
+	}
+	if useW {
+		fmt.Printf("%d ", wordCount)
+	}
+	if useC {
+		fmt.Printf("%d ", byteCount)
+	}
+	if useM {
+		fmt.Printf("%d ", runeCount)
+	}
+	fmt.Println(filename)
+
 }
